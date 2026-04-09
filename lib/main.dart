d1644f8204f217c50acca80faeb787f6d3cde011
AlexBlackHawk <alex.maudza@gmail.com>
2026-04-09 14:37:18 +0300

    feat: adding firebase crashlytics
    Added firebase crashlytics module to record Flutter fatal errors.

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,64 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:ui';
 
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:chefvision/core/configs/config_sdk.dart';
+import 'package:chefvision/core/configs/store_config.dart';
+import 'package:chefvision/core/env/env.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:purchases_flutter/models/store.dart';
 
 import 'app.dart';
-import 'configs/config_sdk.dart';
-import 'configs/store_config.dart';
-import 'env/env.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
-  MobileAds.instance.initialize();
+      await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+      MobileAds.instance.initialize();
 
-  if (Platform.isIOS || Platform.isMacOS) {
-    StoreConfig(
-      store: Store.appStore,
-      apiKey: Env.appleRevenueCatApiKey,
-    );
-  } else if (Platform.isAndroid) {
-    StoreConfig(
-      store: Store.playStore,
-      apiKey: Env.googleRevenueCatApiKey,
-    );
-  }
+      if (!kIsWeb && kDebugMode) {
+        FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+        await FirebaseCrashlytics.instance.deleteUnsentReports();
+      }
 
-  await configureSDK();
+      FlutterError.onError = (errorDetails) {
+        if (!kIsWeb && !kDebugMode) {
+          FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+        }
+      };
 
-  runApp(const App());
+      PlatformDispatcher.instance.onError = (error, stack) {
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+        return true;
+      };
+
+      if (Platform.isIOS || Platform.isMacOS) {
+        StoreConfig(
+          store: Store.appStore,
+          apiKey: Env.appleRevenueCatApiKey,
+        );
+      } else if (Platform.isAndroid) {
+        StoreConfig(
+          store: Store.playStore,
+          apiKey: Env.googleRevenueCatApiKey,
+        );
+      }
+
+      await configureSDK();
+
+      runApp(const App());
+    },
+    (error, stackTrace) {
+      if (!kIsWeb) {
+        FirebaseCrashlytics.instance.recordError(error, stackTrace, fatal: true);
+      }
+    },
+  );
 }
