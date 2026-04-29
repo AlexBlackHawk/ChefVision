d0e4c97a8747b5b795442e35ed673545e3a8e7b2
AlexBlackHawk <alex.maudza@gmail.com>
2026-04-29 15:06:54 +0300

    fix: fixing localization parameters
    Fixing localization parameters in MaterialApp.router by importing easy_localization package.

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:chefvision/presentation/routing/app_router.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
