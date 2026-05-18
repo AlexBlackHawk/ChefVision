8b52e6f387f472d237d9e149e3631681b91f0e06
Alex Maudza <71087168+AlexBlackHawk@users.noreply.github.com>
2026-05-18 12:57:23 +0300

    feat: add localization strings (#31)
    Added localization strings for english and ukrainian language.

--- lib/resources/gen/locale_keys.g.dart
+++ lib/resources/gen/locale_keys.g.dart
@@ -3,5 +3,53 @@
 // ignore_for_file: constant_identifier_names
 
 abstract class  LocaleKeys {
+  static const welcomeBack = 'welcomeBack';
+  static const scanIngredientsDescription = 'scanIngredientsDescription';
+  static const openCamera = 'openCamera';
+  static const viewSubscriptionPackages = 'viewSubscriptionPackages';
+  static const noActiveSubscription = 'noActiveSubscription';
+  static const subscribeToRemoveAds = 'subscribeToRemoveAds';
+  static const detectingProducts = 'detectingProducts';
+  static const back = 'back';
+  static const detectedProducts = 'detectedProducts';
+  static const addProduct = 'addProduct';
+  static const generateRecipe = 'generateRecipe';
+  static const generatingYourRecipe = 'generatingYourRecipe';
+  static const hoursAbbreviation = 'hoursAbbreviation';
+  static const minutesAbbreviation = 'minutesAbbreviation';
+  static const caloriesAbbreviation = 'caloriesAbbreviation';
+  static const ingredients = 'ingredients';
+  static const instructions = 'instructions';
+  static const backToHome = 'backToHome';
+  static const addNewProduct = 'addNewProduct';
+  static const productName = 'productName';
+  static const quantityPlaceholder = 'quantityPlaceholder';
+  static const add = 'add';
+  static const cancel = 'cancel';
+  static const save = 'save';
+  static const removeAds = 'removeAds';
+  static const unlockPremiumFeatures = 'unlockPremiumFeatures';
+  static const month = 'month';
+  static const year = 'year';
+  static const subscribeNow = 'subscribeNow';
+  static const restorePurchases = 'restorePurchases';
+  static const loadingPackages = 'loadingPackages';
+  static const loadingSubscription = 'loadingSubscription';
+  static const manageSubscription = 'manageSubscription';
+  static const noActiveSubscriptionStatus = 'noActiveSubscriptionStatus';
+  static const noActiveSubscriptionYet = 'noActiveSubscriptionYet';
+  static const subscribeToRemoveAdsMessage = 'subscribeToRemoveAdsMessage';
+  static const viewSubscriptionPlans = 'viewSubscriptionPlans';
+  static const activeStatus = 'activeStatus';
+  static const needHelp = 'needHelp';
+  static const visitManagementUrlDescription = 'visitManagementUrlDescription';
+  static const managementUrl = 'managementUrl';
+  static const expirationDate = 'expirationDate';
+  static const purchaseDate = 'purchaseDate';
+  static const customerId = 'customerId';
+  static const subscriptionType = 'subscriptionType';
+  static const active = 'active';
+  static const placeProductsTapToCapture = 'placeProductsTapToCapture';
+  static const makeSureProductsVisible = 'makeSureProductsVisible';
 
 }
