06ee008a32830b836a3eb1c9d6f10d2ac914612e
AlexBlackHawk <alex.maudza@gmail.com>
2026-07-01 23:36:35 +0300

    feat: add camera view
    In this commit camera view was added.

--- lib/presentation/pages/camera/camera.dart
+++ lib/presentation/pages/camera/camera.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:chefvision/core/constants/app_colors.dart';
 import 'package:chefvision/core/constants/enums/opening_camera_status.dart';
 import 'package:chefvision/presentation/pages/camera/widgets/camera_access_denied_view.dart';
@@ -5,6 +6,8 @@ import 'package:chefvision/presentation/pages/camera/widgets/camera_error_view.d
 import 'package:chefvision/presentation/pages/camera/widgets/camera_screen_app_bar.dart';
 import 'package:chefvision/presentation/pages/camera/widgets/camera_screen_content.dart';
 import 'package:chefvision/presentation/pages/camera/widgets/initializing_camera_view.dart';
+import 'package:collection/collection.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CameraPage extends StatefulWidget {
@@ -14,9 +17,112 @@ class CameraPage extends StatefulWidget {
   State<CameraPage> createState() => _CameraPageState();
 }
 
-class _CameraPageState extends State<CameraPage> {
+class _CameraPageState extends State<CameraPage> with WidgetsBindingObserver, TickerProviderStateMixin {
 
-  final OpeningCameraStatus openingCameraStatus = OpeningCameraStatus.opened;
+  CameraController? controller;
+
+  OpeningCameraStatus? openingCameraStatus;
+
+  CameraDescription? backCameraDescription;
+
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _initializeCameraController();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController? cameraController = controller;
+
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      _initializeCameraController(cameraController.description);
+    }
+
+    super.didChangeAppLifecycleState(state);
+  }
+
+  Future<void> _initializeCameraController([CameraDescription? cameraDescription]) async {
+    setState(() {
+      openingCameraStatus = OpeningCameraStatus.initializing;
+    });
+    CameraDescription camera;
+
+    if (cameraDescription == null) {
+      List<CameraDescription> cameras = await availableCameras();
+      backCameraDescription = cameras.firstWhereOrNull((camera) => camera.lensDirection == CameraLensDirection.back);
+
+      if (backCameraDescription == null) {
+        setState(() {
+          openingCameraStatus = OpeningCameraStatus.error;
+        });
+        return;
+      }
+      else {
+        camera = backCameraDescription!;
+      }
+    }
+    else {
+      camera = cameraDescription;
+    }
+
+    final cameraController = CameraController(
+      camera,
+      kIsWeb ? ResolutionPreset.max : ResolutionPreset.medium,
+      enableAudio: false,
+      imageFormatGroup: ImageFormatGroup.jpeg,
+    );
+
+    controller = cameraController;
+
+    cameraController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+      if (cameraController.value.hasError) {
+        setState(() {
+          openingCameraStatus = OpeningCameraStatus.error;
+        });
+      }
+    });
+
+    try {
+      await cameraController.initialize();
+      setState(() {
+        openingCameraStatus = OpeningCameraStatus.opened;
+      });
+    } on CameraException catch (e) {
+      switch (e.code) {
+        case 'CameraAccessDenied' || 'CameraAccessDeniedWithoutPrompt':
+          setState(() {
+            openingCameraStatus = OpeningCameraStatus.accessDenied;
+          });
+        default:
+          setState(() {
+            openingCameraStatus = OpeningCameraStatus.error;
+          });
+      }
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +131,10 @@ class _CameraPageState extends State<CameraPage> {
       appBar: CameraScreenAppBar(),
       extendBodyBehindAppBar: true,
       body: switch (openingCameraStatus) {
-        OpeningCameraStatus.opened => CameraScreenContent(),
-        OpeningCameraStatus.error => CameraErrorView(),
+        OpeningCameraStatus.opened => CameraScreenContent(
+          controller: controller!,
+        ),
+        OpeningCameraStatus.error || null => CameraErrorView(),
         OpeningCameraStatus.accessDenied => CameraAccessDeniedView(),
         OpeningCameraStatus.initializing => InitializingCameraView(),
       },
